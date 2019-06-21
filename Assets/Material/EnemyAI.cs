208d5b880c8a31597de44dbac6b84d63104579ad
kwonsye <kwonsye@naver.com>
2019-06-21 23:50:11 +0900

    following enemy test

---
+++ Assets/Material/EnemyAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyAI : MonoBehaviour {
+	
+	NavMeshAgent nav;
+	GameObject target;
+	
+	// Use this for initialization
+	void Start () {
+		nav = GetComponent<NavMeshAgent>();
+		target = GameObject.Find("Player");
+		Debug.Log (target);
+		//Debug.Log (nav.destination);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (nav.destination != target.transform.position)
+		{
+			//Debug.Log(target.transform.position);
+			nav.SetDestination (target.transform.position);
+		}
+		else
+		{
+			nav.SetDestination (transform.position);
+		}
+	}
+}
